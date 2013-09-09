3a7d583e892214ff1ca628688ca5df44336789c2
brandonjschwartz <brandon@boomajoom.com>
2013-09-08 21:00:45 -0700

    Updates to directories.

--- pharaoh/scripts/pharaoh.py
+++ pharaoh/scripts/pharaoh.py
@@ -1,80 +1,58 @@
-# (c) 2005 Ian Bicking and contributors; written for Paste
-# (http://pythonpaste.org) Licensed under the MIT license:
-# http://www.opensource.org/licenses/mit-license.php
-
 import optparse
 import os
-import os.path
 import pkg_resources
 import re
 import sys
 
 _bad_chars_re = re.compile('[^a-zA-Z0-9_]')
 
+
 def main(argv=sys.argv, quiet=False):
-    command = PCreateCommand(argv, quiet)
+    command = PkgCreateCommand(argv, quiet)
     return command.run()
 
-class PCreateCommand(object):
+
+class PkgCreateCommand(object):
     verbosity = 1 # required
-    description = "Render Pyramid scaffolding to an output directory"
+    description = "Render a new starter package"
     usage = "usage: %prog [options] output_directory"
     parser = optparse.OptionParser(usage, description=description)
-    parser.add_option('-s', '--scaffold',
-                      dest='scaffold_name',
+    parser.add_option('-s', '--starter_package',
+                      dest='package_name',
                       action='append',
-                      help=("Add a scaffold to the create process "
+                      help=("Add a package to the create process "
                             "(multiple -s args accepted)"))
-    parser.add_option('-t', '--template',
-                      dest='scaffold_name',
-                      action='append',
-                      help=('A backwards compatibility alias for '
-                            '-s/--scaffold.  Add a scaffold to the '
-                            'create process (multiple -t args accepted)'))
     parser.add_option('-l', '--list',
                       dest='list',
                       action='store_true',
-                      help="List all available scaffold names")
-    parser.add_option('--list-templates',
-                      dest='list',
-                      action='store_true',
-                      help=("A backwards compatibility alias for -l/--list.  "
-                            "List all available scaffold names."))
+                      help="List all available starter packages")
     parser.add_option('--simulate',
                       dest='simulate',
                       action='store_true',
                       help='Simulate but do no work')
-    parser.add_option('--overwrite',
-                      dest='overwrite',
-                      action='store_true',
-                      help='Always overwrite')
-    parser.add_option('--interactive',
-                      dest='interactive',
-                      action='store_true',
-                      help='When a file would be overwritten, interrogate')
 
     def __init__(self, argv, quiet=False):
         self.quiet = quiet
         self.options, self.args = self.parser.parse_args(argv[1:])
-        self.scaffolds = self.all_scaffolds()
+        self.packages = self.all_packages()
 
     def run(self):
         if self.options.list:
-            return self.show_scaffolds()
-        if not self.options.scaffold_name:
+            return self.show_packages()
+        if not self.options.package_name:
             self.out('You must provide at least one scaffold name')
             return 2
         if not self.args:
             self.out('You must provide a project name')
             return 2
-        available = [x.name for x in self.scaffolds]
-        diff = set(self.options.scaffold_name).difference(available)
+        available = [x.name for x in self.packages]
+        diff = set(self.options.package_name).difference(available)
         if diff:
             self.out('Unavailable scaffolds: %s' % list(diff))
             return 2
-        return self.render_scaffolds()
+        return self.render_packages()
 
-    def render_scaffolds(self):
+    def render_packages(self):
         options = self.options
         args = self.args
         project_name = os.path.basename(args[0])
@@ -87,38 +65,38 @@ class PCreateCommand(object):
             'package': pkg_name,
             'egg': egg_name,
             }
-        for scaffold_name in options.scaffold_name:
-            for scaffold in self.scaffolds:
-                if scaffold.name == scaffold_name:
-                    scaffold.run(self, output_dir, vars)
+        for package_name in options.package_name:
+            for package in self.packages:
+                if package.name == package_name:
+                    package.run(self, output_dir, vars)
         return 0
 
-    def show_scaffolds(self):
-        scaffolds = sorted(self.scaffolds, key=lambda x: x.name)
-        if scaffolds:
-            max_name = max([len(t.name) for t in scaffolds])
+    def show_packages(self):
+        packages = sorted(self.packages, key=lambda x: x.name)
+        if packages:
+            max_name = max([len(t.name) for t in packages])
             self.out('Available scaffolds:')
-            for scaffold in scaffolds:
+            for package in packages:
                 self.out('  %s:%s  %s' % (
-                    scaffold.name,
-                    ' '*(max_name-len(scaffold.name)), scaffold.summary))
+                    package.name,
+                    ' '*(max_name-len(package.name)), package.summary))
         else:
             self.out('No scaffolds available')
         return 0
 
-    def all_scaffolds(self):
-        scaffolds = []
+    def all_packages(self):
+        packages = []
         eps = list(pkg_resources.iter_entry_points('pyramid.scaffold'))
         for entry in eps:
             try:
-                scaffold_class = entry.load()
-                scaffold = scaffold_class(entry.name)
-                scaffolds.append(scaffold)
+                package_class = entry.load()
+                package = package_class(entry.name)
+                packages.append(package)
             except Exception as e: # pragma: no cover
                 self.out('Warning: could not load entry point %s (%s: %s)' % (
                     entry.name, e.__class__.__name__, e))
-        return scaffolds
+        return packages
 
-    def out(self, msg): # pragma: no cover
+    def out(self, msg):  # pragma: no cover
         if not self.quiet:
             print(msg)
