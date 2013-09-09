287133ad3ff91af77d3e2c1a6b03afb5785bcbb9
brandonjschwartz <brandon@boomajoom.com>
2013-09-08 21:23:32 -0700

    pyramid template rendering

--- pharaoh/starter_packages/__init__.py
+++ pharaoh/starter_packages/__init__.py
@@ -2,5 +2,5 @@ from pyramid.scaffolds import PyramidTemplate
 
 
 class SqlAlchemyPkg(PyramidTemplate):
-    template_dir = 'sqlalchemy'
+    _template_dir = 'sqlalchemy'
     summary = 'Sqlalchemy base package.'
